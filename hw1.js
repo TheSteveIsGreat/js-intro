152c7eea740115b1d3c6f77f7844482cf11f22e2
Steve Crass <stevecrass@Saras-MBP.hsd1.ut.comcast.net>
2022-02-15 18:41:32 -0700

    finished homework 1

--- hw1.js
+++ hw1.js
@@ -1,47 +1,47 @@
 // Loops
 // 1. Write a for loop that logs only logs even numbers from 0 - 10; ie 0,2,4,..10
 
-// for (let i = 0; i <= 10; i+=2) {
-//   console.log(i);
-// }
+for (let i = 0; i <= 10; i+=2) {
+  console.log(i);
+}
 
 // 2. Write a for that logs numbers from 10 - 0; ie 10,9,8...0
 
-// for (let i = 10; i >= 0; i--) {
-//   console.log(i);
-// }
+for (let i = 10; i >= 0; i--) {
+  console.log(i);
+}
 
 // 3. write a for loop that loops through this array and logs 'odd/even' based on wether the number is odd even
 
-// let nums = [1,2,3,4,5,6,7,8,9,10]
+let nums = [1,2,3,4,5,6,7,8,9,10]
 
-// nums.forEach((num)=>{
-//   if (num % 2 === 0) {
-//     console.log('even');
-//   } else {
-//     console.log('odd');
-//   }
-// })
+nums.forEach((num)=>{
+  if (num % 2 === 0) {
+    console.log('even');
+  } else {
+    console.log('odd');
+  }
+})
 
 // 4. write a while loop that starts at 0 and counts up to 10 ie: 0,1,2,3....10
 
-// let i = 0
-// while(i <= 10) {
-//   console.log(i);
-//   ++i;
-// }
+let i = 0
+while(i <= 10) {
+  console.log(i);
+  ++i;
+}
 
 // 5. Look closely at the code below
 
-// const isOdd = (num) =>{
-//     return num % 2 === 0
-// }
+const isOdd = (num) =>{
+    return num % 2 === 0
+}
 
-// if(isOdd(2)){
-//     console.log('is Odd')
-// } else {
-//     console.log('is Even')
-// }
+if(isOdd(2)){
+    console.log('is Odd')
+} else {
+    console.log('is Even')
+}
 
 // 6. what will this log?
 // isOdd
@@ -51,71 +51,69 @@
 
 // 8. Fix the isOdd function to make this work
 
-// const isOdd = (num) =>{
-//   return num % 2;
-// }
+const isOdd = (num) =>{
+  return num % 2;
+}
 
-// if(isOdd(2)){
-//   console.log('is Odd')
-// } else {
-//   console.log('is Even')
-// }
+if(isOdd(2)){
+  console.log('is Odd')
+} else {
+  console.log('is Even')
+}
 
 // Functions
 // 1. Write a function that takes a positive number and prints all even numbers from 0 – number
 
-  // const printEvenTo = (num) => {
-  //   for (let i = 0; i <= num; i+=2) {
-  //     console.log(i);
-  //   }
-  // }
+  const printEvenTo = (num) => {
+    for (let i = 0; i <= num; i+=2) {
+      console.log(i);
+    }
+  }
 
 // 2. Write a function that takes an array and returns the sum of all the numbers
 
-// const sumOfAll = (numArray) => {
-// let sum = 0;
+const sumOfAll = (numArray) => {
+let sum = 0;
 
-// for (let i = 0; i < numArray.length; i++) {
-//   sum += numArray[i]
-// }
-//   return sum
-// }
+for (let i = 0; i < numArray.length; i++) {
+  sum += numArray[i]
+}
+  return sum
+}
 
 // 3. Write a function that returns the number of vowels in string
 
-// const vowels = ['a', 'e', 'i', 'o', 'u']
+const vowels = ['a', 'e', 'i', 'o', 'u']
 
-// const numOfVowels = (str) => {
-//   let count = 0;
-//   for (let letter of str.toLowerCase()) {
-//     if (vowels.includes(letter)) {
-//       count++
-//     }
-//   } 
-//   return count
-// }
-
-// console.log(numOfVowels('supercalafragilisticexpealadocious'))
+const numOfVowels = (str) => {
+  let count = 0;
+  for (let letter of str.toLowerCase()) {
+    if (vowels.includes(letter)) {
+      count++
+    }
+  } 
+  return count
+}
 
 // 4. Write a function that takes a math operator, as a string, ('*','/','+','-') and two numbers and returns the result
 
-// const operators = ['*', '/', '+', '-']
+const operators = ['*', '/', '+', '-']
 
-// const mathString = (num1, op, num2) => {
-//   let result;
+const mathString = (num1, op, num2) => {
+  let result;
 
-//   if (op === '*') {
-//     result = num1 * num2
-//   } else if (op === '/') {
-//     result = num1 / num2
-//   } else if (op === '+') {
-//     result = num1 + num2
-//   } else {
-//     result = num1 - num2
-//   }
+  if (op === '*') {
+    result = num1 * num2
+  } else if (op === '/') {
+    result = num1 / num2
+  } else if (op === '+') {
+    result = num1 + num2
+  } else {
+    result = num1 - num2
+  }
 
-//   return result
-// }
+  return result
+}
 
 // 5. Write a function that takes an array of strings and a string and returns true or false if the string is in the array.
 
@@ -136,13 +134,47 @@ const personDescription = (person) => {
 
 let p1 = {firstname: 'Steve', lastname: 'Crass', age: 32}
 
-console.log(personDescription(p1))
 // 7. write a function that takes an array that removes the first and last items and return that new array
+
+const removeFirstAndLast = (arr) => {
+  arr.shift()
+  arr.pop()
+  return arr
+}
+
 // 8. write a function that swaps the last and first items of an array 
+
+const swapFirstAndLast = (arr) => {
+  let first = arr[0], last = arr[arr.length - 1]
+
+  arr.shift()
+  arr.unshift(last)
+  arr.pop()
+  arr.push(first)
+
+  return arr
+}
+
 // 9. write a function that takes a array and a second argument and returns the index in the array of the second argument
+
+const whereIsItem = (arr, item) => {
+  item = arr.indexOf(item)
+  return item
+}
+
 // 10. write a function that takes a number and uses a ternary to return if the number is odd or even.
+
+const oddOrEven = (num) => {
+  return (num % 2 === 0 ? 'even' : 'odd')
+}
+
 // 11. Write a function that prints all  numbers from 0 – 10
 
+const printNumbers = () => {
+  for (let i = 0; i <= 10; i++) {
+    console.log(i)
+  }
+}
 
 
 
