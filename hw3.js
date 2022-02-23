e6f60adbb70d4228db4830e8117b3a9b0dc419b7
Steve Crass <stevecrass@Saras-MBP.hsd1.ut.comcast.net>
2022-02-23 13:25:06 -0700

    did bonus on hw3

--- hw3.js
+++ hw3.js
@@ -19,52 +19,53 @@ const getChange = (amount) =>{
   amount = amount - (nickles * 5)
   pennies = Math.floor(amount / 1)
 
-  return (`${quarters}, ${dimes}, ${nickles}, ${pennies}`)
+  return (`${quarters},${dimes},${nickles},${pennies}`)
 
+  // BONUS:
+  // return ({Quarters: quarters, Nickles: nickles, Dimes: dimes, Pennies: pennies})
 }
 
-console.log(getChange(72))
 
 // NOTE: BELOW CODE IS COMPLETED DON'T CHANGE UNLESS DOING BONUS
 // returns true if ALL tests pass, false if ONE OR MORE test fail
-// testGetChange = () =>{
-//    // Assuming all test passed, but change it to false
-//    // if any fail 
-//    let passed = true
-//    if(getChange(0) !== '0,0,0,0'){
-//        console.log(`failed at: getChange(0) == '0,0,0,0'`)
-//        passed = false
-//    }
-//    if(getChange(1) !== '0,0,0,1'){
-//        console.log(`failed at: getChange(1) == '0,0,0,1' `)
-//        passed = false
-//    }
-//    if(getChange(5) !== '0,0,1,0'){
-//        console.log(`failed at: getChange(5) == '0,0,1,0' `)
-//        passed = false
-//    }
-//    if(getChange(10) !== '0,1,0,0'){
-//        console.log(`failed at: getChange(10) == '0,1,0,0' `)
-//        passed = false
-//    }
-//    if(getChange(25) !== '1,0,0,0'){
-//        console.log(`failed at: getChange(10) == '1,0,0,0' `)
-//        passed = false
-//    }
-//    if(getChange(26) !== '1,0,0,1'){
-//        console.log(`failed at: getChange(26) == '1,0,0,1'`)
-//        passed = false
-//    }
-//    if(getChange(99) !== '3,2,0,4'){
-//        console.log(`failed at: getChange(99) == '3,2,0,4'`)
-//        passed = false
-//    }
-//    return passed
-// }
+testGetChange = () =>{
+   // Assuming all test passed, but change it to false
+   // if any fail 
+   let passed = true
+   if(getChange(0) !== '0,0,0,0'){
+       console.log(`failed at: getChange(0) == '0,0,0,0'`)
+       passed = false
+   }
+   if(getChange(1) !== '0,0,0,1'){
+       console.log(`failed at: getChange(1) == '0,0,0,1' `)
+       passed = false
+   }
+   if(getChange(5) !== '0,0,1,0'){
+       console.log(`failed at: getChange(5) == '0,0,1,0' `)
+       passed = false
+   }
+   if(getChange(10) !== '0,1,0,0'){
+       console.log(`failed at: getChange(10) == '0,1,0,0' `)
+       passed = false
+   }
+   if(getChange(25) !== '1,0,0,0'){
+       console.log(`failed at: getChange(10) == '1,0,0,0' `)
+       passed = false
+   }
+   if(getChange(26) !== '1,0,0,1'){
+       console.log(`failed at: getChange(26) == '1,0,0,1'`)
+       passed = false
+   }
+   if(getChange(99) !== '3,2,0,4'){
+       console.log(`failed at: getChange(99) == '3,2,0,4'`)
+       passed = false
+   }
+   return passed
+}
 
-// if(testGetChange()){
-//    console.log('working good job')
-// }
+if(testGetChange()){
+   console.log('working good job')
+}
 
 // BONUS ADD your own test cases
 // BONUS Write another function that does the same but have it return an object {quartes:0, dimes:0, nickles:0 pennies:0}
